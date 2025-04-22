64b0e8da4db0798ffed929208e3e72a33e302117
sai3791 <69799036+sai3791@users.noreply.github.com>
2025-04-22 08:53:36 +0000

    Added Day-4 Notes

--- Day-4/calculator.py
+++ Day-4/calculator.py
@@ -1,1 +1,8 @@
-
+num1 = 10
+num2 = 2
+addition = num1 + num2
+print("value of addition = " + str(addition))
+sub = num1 - num2
+print(sub)
+mul = num1 * num2
+print(mul)
---
+++ Day-4/calculatorNew.py
@@ -0,0 +1,23 @@
+a = 10
+b = 2
+
+def add():
+    add = a+b
+    print(add)
+
+def sub():
+    sub = a -b
+    print(sub)
+
+def mul():
+    mul = a * b
+    print(mul)
+
+def div():
+    div = a / b
+    print(div)
+
+add()
+sub()
+mul()    
+div()
